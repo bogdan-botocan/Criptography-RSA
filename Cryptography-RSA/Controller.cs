8ec1350e7841f65012725203c3e091018388c04e
Bianca Metzler <bianca.metzi@gmail.com>
2016-01-07 22:59:23 +0200

    Added controller stub and renamed some user interface objects

---
+++ Cryptography-RSA/Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography_RSA
+{
+    public static class Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Text"></param>
+        /// <returns></returns>
+        public static string Encrypt(string Text)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Text"></param>
+        /// <returns></returns>
+        public static string Decrypt(string Text)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// This should not return void
+        /// </summary>
+        public static void GenerateKeyPair()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
