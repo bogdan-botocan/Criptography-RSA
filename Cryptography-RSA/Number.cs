a83679931732e452c4d1e86566aa8eb14056cea3
Bianca Metzler <bianca.metzi@gmail.com>
2016-01-08 01:38:43 +0200

    Implemented GetRandomNumberInIntervalGcdedWithNumberIsOne

--- Cryptography-RSA/Number.cs
+++ Cryptography-RSA/Number.cs
@@ -10,7 +10,7 @@ namespace Cryptography_RSA
     public static class Number
     {
         // private fields
-        private static Random _Random;
+        private static Random _Random = new Random();
 
         /// <summary>
         /// Generates a large prime number
@@ -19,11 +19,32 @@ namespace Cryptography_RSA
         /// <returns> A BigInteger value with Length number of digits </returns>
         public static BigInteger GetPrimeNumber(int Length)
         {
-            random = new Random();
-            byte[] data = new byte[Length];
-            random.NextBytes(data);
+            byte[] data;
+            do
+            {
+                data = new byte[Length];
+                data[0] = 0;
+                _Random.NextBytes(data);
+            } while (MillerRabin.IsPrime(new BigInteger(data)));
 
             return new BigInteger(data);
+        }
+
+        public static BigInteger
+            GetRandomNumberInIntervalGcdedWithNumberIsOne(
+            BigInteger MinValue,
+            BigInteger MaxValue,
+            BigInteger GcdToBeOne)
+        {
+            for (BigInteger wildNumber = MinValue; wildNumber < MaxValue; wildNumber++)
+            {
+                if(BigInteger.GreatestCommonDivisor(wildNumber, GcdToBeOne) == 1)
+                {
+                    return wildNumber;
+                }
+            }
+
+            throw new Exception("Esti retardat!");
         }
     }
 }
