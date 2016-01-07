ab210f5fe7b742f70bdf33492ae5e90568836ecd
Bianca Metzler <bianca.metzi@gmail.com>
2016-01-07 23:26:17 +0200

    Added Number.cs to solution

---
+++ Cryptography-RSA/Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptography_RSA
+{
+    public class Number
+    {
+        /// <summary>
+        /// Generates a large prime number
+        /// </summary>
+        /// <param name="Length"> Number of digits of number to generate </param>
+        /// <returns> A BigInteger value with Length number of digits </returns>
+        public BigInteger GetPrimeNumber(int Length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
